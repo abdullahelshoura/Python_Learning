554e6af5b51d97c8838d6f6138e33e809e9bb5ec
abdullahelshoura <shourauid@gmail.com>
2022-12-17 12:32:22 +0200

    4th commit

---
+++ Variable_Part1.py
@@ -0,0 +1,19 @@
+# ------------------------------------
+# --Variable--
+# ------------
+# Name Convention and Rules
+# [1] Can Start with (a-z A-Z) Or Underscore
+# [2] Cannot Start with Number or Special Character
+# [3] Can include [0-9] or underscore
+# [4] cannot include Special Characters
+# [5] Name is Not like name (Case Sesitive)
+# Syntax => [Variable Name] [Assignment Operatot] [Value]
+
+MyVariable = "Abdullah"
+print(MyVariable)
+
+myVariable = "Mohamed"
+print(myVariable)
+
+_myVariable = "Elshoura"
+print(_myVariable)
