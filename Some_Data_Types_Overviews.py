eabc7a328df210269675762a95999e86b3eddeae
abdullahelshoura <shourauid@gmail.com>
2022-12-17 12:11:04 +0200

    This is My Third Commit

---
+++ Some_Data_Types_Overviews.py
@@ -0,0 +1,11 @@
+# -----------------------------
+# All Data in Python are Object
+# -----------------------------
+# type() show the value type (int,float,string,boolean,....)
+print(type(10))
+print(type(10.9))
+print(type("Hello Python"))
+print(type([1, 2, 3, 4]))  # list []
+print(type((1, 2, 3, 4)))  # Tuple ()
+print(type({"one": 1, "two": 2, "three": 3}))  # Dictionary {"key",value}
+print(type(2 == 4))  # 2==4 checks the condition and returnts boolean (True , False)
