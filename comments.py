a7f5c037e0fc7868347ec96c2c59f2df4f1e3b2b
abdullahelshoura <shourauid@gmail.com>
2022-12-17 11:36:45 +0200

    This is  my Second Commit

---
+++ comments.py
@@ -0,0 +1,7 @@
+# This is Inline comment
+"""This Is not 
+    Multiple lines 
+    Comment this is a string not assigned to any variable 
+    thats why it isn`t printed so its work as an multiple line comment
+    but it is wrong to use .
+"""
